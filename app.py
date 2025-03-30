ec3519add8fc6d2104e0d11f0c8920f1105df7c9
AritrikaDutta <aritrikajal@gmail.com>
2025-03-31 00:31:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the current directory to the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import streamlit as st
 from app.draw_predict import draw_digit_predict
 from app.image_predict import image_digit_predict
